1e9eb05274a470606872624cfa64bb867d7e89c5
Crystal <cforourk@umich.edu>
2026-04-28 16:19:23 -0400

    made beginning files and my diglett array. sad today lets make something cool, yeah?

---
+++ src/data.js
@@ -0,0 +1,50 @@
+const DIGLETTS = [
+  {
+    id: 1,
+    image: "../images/diglett.png",
+    name: "Diglett",
+    points: 1,
+  },
+    {
+    id: 2,
+    image: "../images/dugtrio.png",
+    name: "Dugtrio",
+    points: 5,
+  },
+    {
+    id: 3,
+    image: "../images/AlolaDig.png",
+    name: "Alola Diglett",
+    points: 3,
+  },
+    {
+    id: 4,
+    image: "../images/alolaDugtrio.png",
+    name: "Alola Dugtrio",
+    points: 7,
+  },
+    {
+    id: 5,
+    image: "../images/shinyDiglett.png",
+    name: "Shiny Diglett",
+    points: 9,
+  },
+      {
+    id: 7,
+    image: "../images/shinyDugtrio.png",
+    name: "Shiny Dugtrio",
+    points: 12,
+  },
+  {
+    id: 8,
+    image: "../images/shinyAlolaDig.png",
+    name: "Shiny Alola Diglett",
+    points: 13,
+  },
+  {
+    id: 9,
+    image: "../images/shinyAlolaDugtrio.png",
+    name: "Shiny Alola Dugtrio",
+    points: 17,
+  }
+]
